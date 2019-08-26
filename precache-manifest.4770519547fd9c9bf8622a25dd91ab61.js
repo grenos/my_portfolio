fdb24577450853261ec7569167d3cd7daff7cadb
mcsaatchidevelopers <tools@mcsaatchi-milano.com>
2019-08-26 13:23:03 +0300

    Updates

---
+++ precache-manifest.4770519547fd9c9bf8622a25dd91ab61.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0b23ee0ed7b4064eadffed0f198da6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb0b24d995afbba7491a",
+    "url": "/static/css/2.a21b90e1.chunk.css"
+  },
+  {
+    "revision": "9f51a1f41c31c1a2ba38",
+    "url": "/static/css/4.ba8b38c6.chunk.css"
+  },
+  {
+    "revision": "36783278ce8f01767a24",
+    "url": "/static/css/5.1c78f635.chunk.css"
+  },
+  {
+    "revision": "86239a01e274d4b56862",
+    "url": "/static/css/main.a08858e9.chunk.css"
+  },
+  {
+    "revision": "cb0b24d995afbba7491a",
+    "url": "/static/js/2.9fc67cff.chunk.js"
+  },
+  {
+    "revision": "c1df71307c62cdb50249",
+    "url": "/static/js/3.eb0155d1.chunk.js"
+  },
+  {
+    "revision": "9f51a1f41c31c1a2ba38",
+    "url": "/static/js/4.a5d85072.chunk.js"
+  },
+  {
+    "revision": "36783278ce8f01767a24",
+    "url": "/static/js/5.44f8f2cb.chunk.js"
+  },
+  {
+    "revision": "02719226eda2d655caee",
+    "url": "/static/js/6.723a9ad7.chunk.js"
+  },
+  {
+    "revision": "6394f36e2dee7bdbc518",
+    "url": "/static/js/7.ceee64b5.chunk.js"
+  },
+  {
+    "revision": "86239a01e274d4b56862",
+    "url": "/static/js/main.85c8f043.chunk.js"
+  },
+  {
+    "revision": "43080497be151f3408d3",
+    "url": "/static/js/runtime~main.9083f4c5.js"
+  }
+]);
